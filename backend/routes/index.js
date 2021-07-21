03cef40d7d19404f5b62af8994c0fd2dc661380c
Carlos Gonzalez <carlos.gonzalez_arce@spaceforce.mil>
2021-07-21 14:26:48 -0600

    add PATCH backend route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -21,20 +21,34 @@ router.get('/created', (req, res) => {
     .catch(err => console.log(err))
 })
 router.post('/new-quote', (req, res) => {
+  console.log('POST started')
   knex('quotes')
     .insert(req.body)
     .then(data => res.status(200).send('Quote created'))
     .catch(err => console.log(err))
 })
 
+router.patch('/created', (req, res) => {
+  console.log('PATCH started')
+  knex('quotes')
+    .where({
+      id: req.body.id,
+      user_created: true
+    })
+    .update(req.body)
+    .then(() => res.status.(200).send('Updated'))
+    .catch(err => console.log(err))
+})
+
 router.delete('/', (req, res) => {
+  console.log('DELETE started')
   knex('quotes')
     .where({
       id: req.body.id,
       user_created: true
     })
     .del()
-    .then(()=> res.status(200).send("Quote deleted"))
+    .then(() => res.status(200).send("Quote deleted"))
     .catch(err => console.log(err))
 })
 
