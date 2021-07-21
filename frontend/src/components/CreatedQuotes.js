050e74dbc2772672c4a11b0633511b897aaf6a2b
Carlos Gonzalez <carlos.gonzalez_arce@spaceforce.mil>
2021-07-20 18:05:57 -0600

    add map function

--- frontend/src/components/CreatedQuotes.js
+++ frontend/src/components/CreatedQuotes.js
@@ -1,8 +1,26 @@
+import { useState, useEffect } from 'react'
 
 
 const CreatedQuotes = () => {
+  const [quotes, setQuotes] = useState([1,2,3]);
 
-  return ('Created Quotes Component')
+  useEffect(() => {
+    console.log('fetching created quotes')
+    fetch('http://localhost:3001/created')
+      .then(data => data.json())
+      .then(json => setQuotes(json))
+      .catch(err => console.log(err))
+  }, [])
+
+  const getQuotes = () => {
+    return quotes.map((quote, index) => {
+        return <p key={index}>"{quote.quote}", -{quote.author}</p>
+      })
+  }
+
+  return (<>
+    {getQuotes()}
+  </>)
 }
 
 export default CreatedQuotes;
